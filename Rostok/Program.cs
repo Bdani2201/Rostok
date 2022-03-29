b9343a162e2a02094582f06aa2023c2f57af2cf2
Bdani2201 <bdani2201@gmail.com>
2022-03-29 12:37:08 +0200

    Add project files.

---
+++ Rostok/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Rostok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.
+            List<Rost> rostok = new List<Rost>();
+            foreach (var sor in File.ReadAllLines("rostok.txt").Skip(1)) rostok.Add(new Rost(sor));
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rostok/rostok.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rostok
+{
+    class Rost
+    {
+        public string Név { get; set; }
+        public string Kategória { get; set; }
+        public string Egység { get; set; }
+        public double RostInGr { get; set; }
+
+        public Rost(string sor)
+        {
+            string[] s = sor.Split(';');
+            Név = s[0];
+            Kategória = s[1];
+            Egység = s[2];
+            RostInGr = double.Parse(s[3]);
+        }
+    }
+}
+
